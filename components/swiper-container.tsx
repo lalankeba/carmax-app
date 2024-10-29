af53ec4fa1745a91bc1a0d9edee305a5e5b76898
lalankeba <lalankeba@gmail.com>
2024-10-29 08:07:27 +0530

    updating main swiper

--- components/swiper-container.tsx
+++ components/swiper-container.tsx
@@ -3,55 +3,60 @@ import { useTranslations } from "next-intl";
 import Image from "next/image";
 import slidePic1 from '@/public/images/swiper/1.png';
 import slidePic2 from '@/public/images/swiper/2.jpg';
-import slidePic3 from '@/public/images/swiper/7.jpg';
+import slidePic3 from '@/public/images/swiper/3.jpg';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, EffectFade, Navigation, Pagination } from 'swiper/modules';
+import { Autoplay, EffectFade } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/effect-fade';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
 import "./swiper-container.scss";
-
 
 const SwiperContainer: React.FC = () => {
   const t = useTranslations('components.SwiperContainer');
+
+  const slides = [
+    {
+      title: t('slide1.title'),
+      subTitle: t('slide1.subTitle'),
+      picture: slidePic1
+    },
+    {
+      title: t('slide2.title'),
+      subTitle: t('slide2.subTitle'),
+      picture: slidePic2
+    },
+    {
+      title: t('slide3.title'),
+      subTitle: t('slide3.subTitle'),
+      picture: slidePic3
+    }
+  ];
 
   return (
     <>
       <div className="swiper-con">
       <Swiper
+        slidesPerView={1}
         spaceBetween={30}
-        effect={'slide'}
+        effect={'fade'}
+        loop={true}
         navigation={false}
         autoplay={{
           delay: 3000,
           disableOnInteraction: false,
         }}
         pagination={false}
-        modules={[Autoplay, EffectFade, Navigation, Pagination]}
+        modules={[Autoplay, EffectFade]}
         className="MainSwiper"
       >
-        <SwiperSlide>
-          <Image src={slidePic1} alt="slide 1" fill={true} style={{ objectFit: 'cover' }} />
-          <div className="slide-text">
-            <h1 style={{ fontSize: '3.5rem', fontWeight: 'bolder' }}>{t('slide1.title')}</h1>
-            <h2>{t('slide1.subTitle')}</h2>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image src={slidePic2} alt="slide 2" fill={true} style={{ objectFit: 'cover' }} />
-          <div className="slide-text">
-            <h1 style={{ fontSize: '3.5rem', fontWeight: 'bolder' }}>{t('slide2.title')}</h1>
-            <h2>{t('slide2.subTitle')}</h2>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image src={slidePic3} alt="slide 3" fill={true} style={{ objectFit: 'cover' }} />
-          <div className="slide-text">
-            <h1 style={{ fontSize: '3.5rem', fontWeight: 'bolder' }}>{t('slide3.title')}</h1>
-            <h2>{t('slide3.subTitle')}</h2>
-          </div>
-        </SwiperSlide>
+        {slides.map((slide) => (
+          <SwiperSlide key={slide.title}>
+            <Image src={slide.picture} alt={slide.title} fill={true} style={{ objectFit: 'cover' }} />
+            <div className="slide-text">
+              <h1 style={{ fontSize: '3.5rem', fontWeight: 'bolder' }}>{slide.title}</h1>
+              <h2>{slide.subTitle}</h2>
+            </div>
+          </SwiperSlide>
+        ))}
       </Swiper>
       </div>
     </>
